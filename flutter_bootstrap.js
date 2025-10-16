73df48c1d1283da6d4c5a40326eb56edae90c2a3
Ahmad Arnaoute <atoddevelopers3@gmail.com>
2025-10-16 19:14:43 +0300

    Built web
    Branch: master
    Commit: 29d8d4ae2a4fe2b73d69e10715d8149cfee986ec (dirty)
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4177647263"
+    serviceWorkerVersion: "1426275989"
   }
 });
