9d2e6753fda1fe69a5c7a205a6f36d7bffbceb96
Ahmad1827 <ahmadarnaoute1896@gmail.com>
2026-05-01 20:55:03 +0300

    Fix Jekyll ignoring .env

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2173367519"
+    serviceWorkerVersion: "4107519572"
   }
 });
