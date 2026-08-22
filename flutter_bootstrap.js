ae55c5d94cbed33a3d7427c23dfaf2a4e9bcef0b
Ahmad1827 <ahmadarnaoute1896@gmail.com>
2026-08-22 21:25:52 +0300

    Add colorful badges, taller cards, more spacing and watermark

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1651709803" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2922367379" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
