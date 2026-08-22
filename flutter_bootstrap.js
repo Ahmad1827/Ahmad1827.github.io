caac44d6098231adadc064cbfbf2c7f4e845458d
Ahmad1827 <ahmadarnaoute1896@gmail.com>
2026-08-22 22:50:21 +0300

    Switch screens to dynamic AppColors

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1851693267" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2345998227" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
