7262b660e34a246f287da08eb28ccc09b528a534
Ahmad1827 <ahmadarnaoute1896@gmail.com>
2026-08-23 16:10:17 +0300

    Fix syntax structure and const evaluations in chat_screen

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3797875154" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1540413140" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
