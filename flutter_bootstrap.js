c3bc84559deb69a991bd66757a27c97aa44c1357
Ahmad1827 <ahmadarnaoute1896@gmail.com>
2026-08-23 18:23:28 +0300

    Clean and symmetrical Retro Hero cards for Home and Exercises screens

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "136251035" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3368717214" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
