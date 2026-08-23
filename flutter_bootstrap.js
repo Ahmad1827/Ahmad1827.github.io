321245742ec4e088ec776456e82dab35ba7a9e39
Ahmad1827 <ahmadarnaoute1896@gmail.com>
2026-08-23 11:01:55 +0300

    Fix blank screen on startup by protecting Firebase calls in main.dart

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2542962464" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1048224325" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
